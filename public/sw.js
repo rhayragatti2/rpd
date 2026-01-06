544ff6b08f641a383b21791ec976d4b0d9584358
Rhayra Gatti <rhayra@yahoo.com.br>
2026-01-06 19:46:54 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,22 @@
 // public/sw.js
-self.addEventListener('push', (event) => {
-  const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: 'https://cdn-icons-png.flaticon.com/512/3665/3665923.png'
-  });
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  console.log('SW Instalado');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('SW Ativado');
+});
+
+// Escuta comandos do App principal
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+    self.registration.showNotification(event.data.title, {
+      body: event.data.body,
+      icon: 'https://cdn-icons-png.flaticon.com/512/3665/3665923.png',
+      badge: 'https://cdn-icons-png.flaticon.com/512/3665/3665923.png',
+      vibrate: [200, 100, 200],
+      tag: 'lembrete-diario' // Evita notificações duplicadas
+    });
+  }
 });
