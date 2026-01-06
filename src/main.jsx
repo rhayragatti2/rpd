05d60bb6272fee6a072673beb695ab720074b19d
Rhayra Gatti <rhayra@yahoo.com.br>
2026-01-06 19:28:54 -0300

    Update main.jsx
    Service Worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
+// Renderização do React
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Registro do Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
-      console.log('Service Worker registrado!', reg);
-    }).catch(err => {
-      console.log('Erro ao registrar Service Worker', err);
-    });
+    // O caminho '/sw.js' assume que o arquivo está dentro da pasta 'public'
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((reg) => {
+        console.log('Service Worker registrado com sucesso! Escopo:', reg.scope);
+      })
+      .catch((err) => {
+        console.error('Falha ao registrar o Service Worker:', err);
+      });
   });
 }
