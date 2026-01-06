f88fbcdd377365f28a1e700d33fbd4d4ea8e8d9c
Rhayra Gatti <rhayra@yahoo.com.br>
2026-01-06 19:27:49 -0300

    Update main.jsx
    ServiceWorker

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(reg => {
+      console.log('Service Worker registrado!', reg);
+    }).catch(err => {
+      console.log('Erro ao registrar Service Worker', err);
+    });
+  });
+}
